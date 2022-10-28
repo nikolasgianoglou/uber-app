92d410760ff1ca5ef8ae8f08006b7c8a8f578d22
nikolasgianoglou <nikolasgcoelho@gmail.com>
2022-10-28 12:16:14 -0300

    Create Users

--- Uber/Controller/SignUpController.swift
+++ Uber/Controller/SignUpController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpController: UIViewController {
     //MARK: - Properties
@@ -68,6 +69,7 @@ class SignUpController: UIViewController {
         let button = AuthButton(type: .system)
         button.setTitle("Sign Up", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
         return button
     }()
     
@@ -96,6 +98,31 @@ extension SignUpController{
     @objc
     private func handleShowLogin() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc
+    private func handleSignUp() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let fullname = fullNameTextField.text else { return }
+        let accountTypeIndex = accountTypeSegmentedControl.selectedSegmentIndex
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authDataResult, error) in
+            if let error = error {
+                print("Failed to register user with error \(error)")
+                return
+            }
+            
+            guard let uid = authDataResult?.user.uid else { return }
+            
+            let values = ["email": email,
+                          "fullname": fullname,
+                          "accountType": accountTypeIndex] as [String: Any]
+            Database.database().reference().child("users").child(uid).updateChildValues(values) { (error, dataBaseReference) in
+                print("Registered user with success")
+            }
+            
+        }
     }
 }
 
